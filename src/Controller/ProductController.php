7b0483c8d3e2cb53e14af902b5aa35a96ec623ca
Yassine <y.aabidouche@gmail.com>
2019-06-25 14:50:15 +0200

    creation de show + create

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * Affiche le détail d'un produit
+     * @Route (
+     *     "/produit/{slug}",
+     *     requirements={"slug"="[a-z0-9\-]+"})
+     * @return Response
+     */
+    public function show(string $slug): Response
+    {
+        var_dump($slug);
+        dump($slug);
+
+        return $this->render('product/show.html.twig');
+    }
+
+    /**
+     * Affiche le détail de "create"
+     * @Route (
+     *     "/produit/create",
+     *     methods={"GET", "POST"})
+     * @param string $slug
+     * @return Response
+     */
+    public function create(Request $requestHTTP): Response
+    {
+        dump($requestHTTP->request);
+        return $this->render('product/create.html.twig');
+    }
+}
